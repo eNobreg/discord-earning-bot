a51341d02e7a3348844d45358e118a3e2b077361
eNobreg <ezra.nobrega@outlook.com>
2022-01-25 03:06:15 -0500

    Basic ORM and guild configuration

---
+++ src/events/GuildCreateEvent.ts
@@ -0,0 +1,37 @@
+// https://discord.js.org/#/docs/main/stable/class/Client?scrollTo=e-guildCreate
+import { Guild } from 'discord.js';
+import BaseEvent from '../utils/structures/BaseEvent';
+import DiscordClient from '../client/client';
+import { getRepository } from 'typeorm';
+import { GuildConfiguration } from '../typeorm/entities/GuildConfiguration';
+
+export default class GuildCreateEvent extends BaseEvent {
+	constructor
+	(
+		private readonly guildConfigRepository = getRepository(GuildConfiguration)
+	)
+	{
+		super('guildCreate');
+	}
+
+	async run(client: DiscordClient, guild: Guild) {
+		console.log('Hello World');
+
+		const config = await this.guildConfigRepository.findOne({
+			guildID: guild.id,
+		});
+
+		if (config) 
+		{
+			console.log("A configuration was found!");
+		} 
+		else
+		{
+			console.log("A configuration was NOT found. Creating one");
+			const newConfig = this.guildConfigRepository.create({
+				guildID: guild.id,
+			});
+			return this.guildConfigRepository.save(newConfig);
+		}
+	}
+}
--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,35 @@
 require ('dotenv').config();
+
+import 'reflect-metadata';
+
 import { registerCommands, registerEvents } from './utils/registry';
 import config from '../slappey.json';
 import DiscordClient from './client/client';
 import { Intents } from 'discord.js';
+import { createConnection } from 'typeorm';
+import { GuildConfiguration } from './typeorm/entities/GuildConfiguration';
+
+
 const client = new DiscordClient({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES ] });
 
 (async () => {
-  client.prefix = config.prefix || client.prefix;
-  await registerCommands(client, '../commands');
-  await registerEvents(client, '../events');
-  await client.login(process.env.DJS_BOT_TOKEN);
+
+	await createConnection({
+		type: 'mysql',
+		host: process.env.DB_HOST,
+		port: 3306,
+		username: process.env.DB_USERNAME,
+		password: process.env.DB_PASSWORD,
+		database: process.env.DB_NAME,
+		synchronize: true,
+		entities: [GuildConfiguration],
+	});
+
+	client.prefix = config.prefix || client.prefix;
+	await registerCommands(client, '../commands');
+	await registerEvents(client, '../events');
+	await client.login(process.env.DJS_BOT_TOKEN);
+
+
 })();
 
